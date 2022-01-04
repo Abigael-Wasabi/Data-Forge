9de732c8e154a7d5859d95d464c16fa5592d9db5
Abigael-Wasabi <94919325+Abigael-Wasabi@users.noreply.github.com>
2022-01-04 15:29:38 +0300

    Update hire_purchase.py

--- hire_purchase.py
+++ hire_purchase.py
@@ -3,12 +3,3 @@ def hp(d,mi,m):
     print(hp)
     
 hp(d = 5000, mi = 200, m = 10)
-
-
-
-
-def alpha(sl,el,st): 
-    for c in range(sl,el,st):
-        yield chr(c)
-
-print(set(alpha("d","t",2)))
